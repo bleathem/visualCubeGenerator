50eea7823f09cb713b9a3314b1112976bc87f1d3
Brian Leathem <bleathem@gmail.com>
2014-09-16 14:20:31 -0700

    Added a modal for the scramble timer

--- mobile/www/js/cube-controllers.js
+++ mobile/www/js/cube-controllers.js
@@ -13,10 +13,34 @@ angular.module('cube', [])
     $scope.friend = Friends.get($stateParams.friendId);
   })
 
-  .controller('ScrambleCtrl', ["$scope", "$stateParams", "$location", "Scrambles", function ($scope, $stateParams, $location, scrambles) {
+  .controller('ScrambleCtrl', ["$scope", "$stateParams", "$location", "$ionicModal", "Scrambles", function ($scope, $stateParams, $location, $ionicModal, scrambles) {
     if (scrambles.length === 0) {
       $location.path("/tab/scrambles")
     }
     $scope.scramble = scrambles.get($stateParams.scrambleId);
-  }])
-;
+
+    $ionicModal.fromTemplateUrl('/templates/timer-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+    }]);
