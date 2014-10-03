a407405100cee7b4f3c33d9840b83a2231b6ca9b
Brian Leathem <bleathem@gmail.com>
2014-10-03 12:28:28 -0700

    Re-factored the oauth-google-installed-client code to be more promise-based

--- mobile/www/components/oauth/oauth-google-installed-client.js
+++ mobile/www/components/oauth/oauth-google-installed-client.js
@@ -3,55 +3,64 @@
   angular.module('oauth.google.installedClient', ['oauth.google.config'])
 
   .factory('googleTokenPromiseInstalled', ['$http', '$q', 'installedClientConfig', 'transformRequestAsFormPost', function($http, $q, installedClientConfig, transformRequestAsFormPost) {
-    var getTokenPromise = function(authWindow) {
-      var authConfig = installedClientConfig;
+    var authConfig = installedClientConfig;
+    /** Get the access code from the google oauth2 provider **/
+    var getCodePromise = function(authWindow) {
       var deferred = $q.defer();
       angular.element(authWindow).on('loadstart', function(e) {
-          var url = e.url;
-          var params = {};
-          var queryString = url.split('?')[1];
-          var regex = /([^&=]+)=([^&]*)/g, m;
-          while ((m = regex.exec(queryString)) !== null) {
-            params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-          }
+        var url = e.url;
+        var params = {};
+        var queryString = url.split('?')[1];
+        var regex = /([^&=]+)=([^&]*)/g, m;
+        while ((m = regex.exec(queryString)) !== null) {
+          params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+        }
+        if (params.code) {
+          authWindow.close();
+          deferred.resolve(params.code);
+        } else if (params.error) {
+          authWindow.close();
+          deferred.reject(params.error);
+        }
+      });
+      return deferred.promise;
+    };
 
-          if (params.code || params.error) {
-            authWindow.close();
-          }
+    /** Exchange the authorization code for an access token **/
+    var getTokenPromise = function(code) {
+      var deferred = $q.defer();
+      /*jshint camelcase:false*/
+      $http({
+        method: 'post',
+        transformRequest: transformRequestAsFormPost,
+        url: authConfig.token_uri,
+        data: {
+          code: code,
+          client_id: authConfig.client_id,
+          client_secret: authConfig.client_secret,
+          redirect_uri: authConfig.redirect_uris[0],
+          grant_type: 'authorization_code'
+        }
+      /*jshint camelcase:true*/
+      }).success(function(data) {
+          deferred.resolve(data);
+      }).error(function(data) {
+          deferred.reject(data);
+      });
+      return deferred.promise;
+    };
 
-          if (params.code) {
-              //Exchange the authorization code for an access token
-              $http({
-                method: 'post',
-                transformRequest: transformRequestAsFormPost,
-                /*jshint camelcase:false*/
-                url: authConfig.token_uri,
-                data: {
-                  code: params.code,
-                  client_id: authConfig.client_id,
-                  client_secret: authConfig.client_secret,
-                  redirect_uri: authConfig.redirect_uris[0],
-                  grant_type: 'authorization_code'
-                }
-                /*jshint camelcase:true*/
-              }).success(function(data) {
-                  deferred.resolve(data);
-              }).error(function(data) {
-                  authWindow.close();
-                  deferred.reject(data);
-              });
-          } else if (params.error) {
-              //The user denied access to the app
-              authWindow.close();
-              deferred.reject({
-                  error: params.error
-              });
-          }
+    return function(authWindow) {
+      var deferred = $q.defer();
+      getCodePromise(authWindow).then(function(code) {
+        getTokenPromise(code).then(function(data) {
+          deferred.resolve(data);
+        }, function(error) {
+          deferred.reject(error);
         });
-
-        return deferred.promise;
-      };
-    return getTokenPromise;
+      });
+      return deferred.promise;
+    };
   }])
 
   .factory('transformRequestAsFormPost', function() {
